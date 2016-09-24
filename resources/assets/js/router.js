6af784ed7bca4fb5fd536b7a2103521fb2042764
Fareez Ahamed <fareez.ahamed@gmail.com>
2016-09-24 12:43:43 +0530

    Login and dashboard working

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -5,16 +5,16 @@ Vue.use(VueRouter)
 
 let router = new VueRouter()
 
-router.beforeEach(transition => {
-  if (!transition.to.router.app.userHasLoggedIn &&
-      transition.to.path !== '/login') {
-    console.log('Hi')
-    transition.redirect({
-      path: '/login'
-    })
-  } else {
-    transition.next()
-  }
-})
+// router.beforeEach(transition => {
+//   if (!transition.to.router.app.userHasLoggedIn &&
+//       transition.to.path !== '/login') {
+//     console.log('Hi')
+//     transition.redirect({
+//       path: '/login'
+//     })
+//   } else {
+//     transition.next()
+//   }
+// })
 
 export default router
