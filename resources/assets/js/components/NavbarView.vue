df074ce23d68bbb229ad9655aaedd1dc33ef2a71
Fareez Ahamed <fareez.ahamed@gmail.com>
2016-09-24 12:16:03 +0530

    .js, .vue added to imports :(

--- resources/assets/js/components/NavbarView.vue
+++ resources/assets/js/components/NavbarView.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-import Navbar from './Navbar'
+import Navbar from './Navbar.vue'
 
 export default {
 
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,9 +1,9 @@
 // import Vue from 'vue'
 import router from './router.js'
-import App from './App'
-import Login from './Login'
-import NavbarView from './components/NavbarView'
-import Dashboard from './components/Dashboard'
+import App from './App.vue'
+import Login from './Login.vue'
+import NavbarView from './components/NavbarView.vue'
+import Dashboard from './components/Dashboard.vue'
 // import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss'
 
 router.map({
