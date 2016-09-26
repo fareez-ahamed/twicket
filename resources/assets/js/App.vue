2897727777129d723f155238d728220dc4f25796
Fareez Ahamed <fareez.ahamed@gmail.com>
2016-09-26 06:43:53 +0530

    Removed Vuex and Login component

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -3,25 +3,10 @@
 </template>
 
 <script>
-import store from './vuex/store'
-import { login } from './vuex/actions'
-
 export default {
 
-  replace: false,
+  replace: false
 
-  store,
-
-  vuex: {
-    getters: {
-      userHasLoggedIn: (state) => {
-        return state.token !== null
-      }
-    },
-    actions: {
-      login: login
-    }
-  }
 }
 </script>
 
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -4,16 +4,12 @@ window.$ = window.jQuery = $
 // import Vue from 'vue'
 import router from './router.js'
 import App from './App.vue'
-import Login from './Login.vue'
 import NavbarView from './components/NavbarView.vue'
 import Dashboard from './components/Dashboard.vue'
 
 require('bootstrap-sass') // ES6 Import doesn't work
 
 router.map({
-  '/login': {
-    component: Login
-  },
   '/': {
     component: NavbarView,
     subRoutes: {
